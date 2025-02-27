9743ba3a508afdde3f4a330b019f368a06c8e831
Gauthier Leonard <gauthier@e36knots.com>
2025-02-28 00:17:16 +0100

    feat(bond): add CorporateBond contracts

---
+++ src/CorporateBond/CorporateBondRepayVault.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+
+pragma solidity 0.8.25;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+/**
+ * @title CorporateBondRepayVault
+ * @notice A vault that allows a borrower to repay a corporate bond to the lender.
+ * @notice The lender is identified by owning the CorporateBond NFT.
+ * @dev This contract is a ERC4626 vault with additional access control.
+ * @dev Only the borrower can deposit assets into the vault and the recipient has to be the lender.
+ * @dev Only the lender can redeem the vault's assets.
+ */
+contract CorporateBondRepayVault is ERC4626 {
+    error OnlyBorrower(address addr, address borrower);
+    error OnlyLender(address addr, address lender);
+    error OnlyLenderRecipient(address addr, address lender);
+
+    address public immutable borrower;
+    IERC721 public immutable bondNFTContract;
+    uint256 public immutable bondNFTTokenId;
+
+    constructor(
+        IERC20 asset,
+        address borrower_,
+        IERC721 bondNFTContract_,
+        uint256 bondNFTTokenId_
+    ) ERC4626(asset) ERC20("CorporateBondRepayVault", "CBRV") {
+        borrower = borrower_;
+        bondNFTContract = bondNFTContract_;
+        bondNFTTokenId = bondNFTTokenId_;
+    }
+
+    function _deposit(
+        address caller,
+        address receiver,
+        uint256 assets,
+        uint256 shares
+    ) internal override {
+        if (caller != borrower) {
+            revert OnlyBorrower(caller, borrower);
+        }
+        if (!_isLender(receiver)) {
+            revert OnlyLenderRecipient(receiver, bondNFTContract.ownerOf(bondNFTTokenId));
+        }
+
+        super._deposit(caller, receiver, assets, shares);
+    }
+
+    function _withdraw(
+        address caller,
+        address receiver,
+        address owner,
+        uint256 assets,
+        uint256 shares
+    ) internal override {
+        if (!_isLender(caller)) {
+            revert OnlyLender(caller, bondNFTContract.ownerOf(bondNFTTokenId));
+        }
+
+        super._withdraw(caller, receiver, owner, assets, shares);
+    }
+
+    function _isLender(
+        address addr
+    ) internal view returns (bool) {
+        return bondNFTContract.ownerOf(bondNFTTokenId) == addr;
+    }
+}
---
+++ test/CorporateBondRepayVaultTest.t.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.25;
+
+import {CorporateBond} from "../src/CorporateBond/CorporateBond.sol";
+import {CorporateBondRepayVault} from "../src/CorporateBond/CorporateBondRepayVault.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock Token", "MTK") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
+
+contract CorporateBondRepayVaultTest is Test {
+    CorporateBond public bond;
+    CorporateBondRepayVault public vault;
+    MockERC20 public token;
+
+    address public borrower;
+    address public lender;
+    uint256 public tokenId;
+
+    function setUp() public {
+        borrower = makeAddr("borrower");
+        lender = makeAddr("lender");
+
+        // Deploy mock ERC20 token
+        token = new MockERC20();
+
+        // Deploy CorporateBond NFT
+        bond = new CorporateBond(address(this));
+        tokenId = bond.safeMint(lender, "test-uri");
+
+        // Deploy vault
+        vault = new CorporateBondRepayVault(token, borrower, bond, tokenId);
+    }
+
+    function testBorrowerCanDeposit() public {
+        uint256 depositAmount = 1000e18;
+        token.mint(borrower, depositAmount);
+
+        vm.startPrank(borrower);
+        token.approve(address(vault), depositAmount);
+        vault.deposit(depositAmount, lender);
+        vm.stopPrank();
+
+        assertEq(vault.balanceOf(lender), depositAmount);
+    }
+
+    function testNonBorrowerCannotDeposit() public {
+        uint256 depositAmount = 1000e18;
+        token.mint(address(this), depositAmount);
+
+        token.approve(address(vault), depositAmount);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                CorporateBondRepayVault.OnlyBorrower.selector, address(this), borrower
+            )
+        );
+        vault.deposit(depositAmount, lender);
+    }
+
+    function testLenderCanWithdraw() public {
+        uint256 depositAmount = 1000e18;
+        token.mint(borrower, depositAmount);
+
+        // Borrower deposits
+        vm.startPrank(borrower);
+        token.approve(address(vault), depositAmount);
+        vault.deposit(depositAmount, lender);
+        vm.stopPrank();
+
+        // Lender withdraws
+        vm.startPrank(lender);
+        vault.withdraw(depositAmount, lender, lender);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(lender), depositAmount);
+    }
+
+    function testNonLenderCannotWithdraw() public {
+        uint256 depositAmount = 1000e18;
+        token.mint(borrower, depositAmount);
+
+        // Borrower deposits
+        vm.startPrank(borrower);
+        token.approve(address(vault), depositAmount);
+        vault.deposit(depositAmount, lender);
+        vm.stopPrank();
+
+        // Non-lender tries to withdraw
+        address nonLender = makeAddr("nonLender");
+        vm.startPrank(nonLender);
+        vm.expectRevert(
+            abi.encodeWithSelector(CorporateBondRepayVault.OnlyLender.selector, nonLender, lender)
+        );
+        vault.withdraw(depositAmount, nonLender, lender);
+        vm.stopPrank();
+    }
+
+    function testDepositToNonLenderFails() public {
+        uint256 depositAmount = 1000e18;
+        token.mint(borrower, depositAmount);
+        address nonLender = makeAddr("nonLender");
+
+        vm.startPrank(borrower);
+        token.approve(address(vault), depositAmount);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                CorporateBondRepayVault.OnlyLenderRecipient.selector, nonLender, lender
+            )
+        );
+        vault.deposit(depositAmount, nonLender);
+        vm.stopPrank();
+    }
+
+    function testLenderTransferUpdatesAccess() public {
+        uint256 depositAmount = 1000e18;
+        token.mint(borrower, depositAmount);
+        address newLender = makeAddr("newLender");
+
+        // Initial deposit
+        vm.startPrank(borrower);
+        token.approve(address(vault), depositAmount);
+        vault.deposit(depositAmount, lender);
+        vm.stopPrank();
+
+        // Transfer NFT to new lender
+        vm.prank(lender);
+        bond.transferFrom(lender, newLender, tokenId);
+
+        // Original lender should no longer be able to withdraw
+        vm.startPrank(lender);
+        vm.expectRevert(
+            abi.encodeWithSelector(CorporateBondRepayVault.OnlyLender.selector, lender, newLender)
+        );
+        vault.withdraw(depositAmount, lender, lender);
+        vm.stopPrank();
+    }
+}
